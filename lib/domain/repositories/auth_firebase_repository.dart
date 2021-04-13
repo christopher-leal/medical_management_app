e22409a98d8e68d67871335171caf2b589363ed2
Christopher Leal <chris_t_o@hotmail.com>
2021-04-12 23:37:34 -0500

    file renamed

--- lib/domain/repositories/auth_firebase_repository.dart
+++ lib/domain/repositories/auth_firebase_repository.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-abstract class AuthFirebaseRepository {
+abstract class AuthRepository {
   Future<bool> login({String email, String password});
   Future<void> logout();
   Future<bool> signup({String email, String password});
