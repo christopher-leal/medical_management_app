da6a608b2f96423dbf35e739f37aa20eb3e1865b
Christopher Leal <chris_t_o@hotmail.com>
2021-04-12 23:38:31 -0500

    dependency injection added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medical_management_app/data/repositories/auth_service.dart';
+import 'package:medical_management_app/domain/repositories/auth_firebase_repository.dart';
+import 'package:medical_management_app/presentation/pages/splash/splash_cubit.dart';
 import 'package:medical_management_app/presentation/pages/splash/splash_screen.dart';
 
 import 'config/theme/theme.dart';
@@ -13,11 +17,17 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      title: 'MedicalManagementApp',
-      home: SplashScreen(),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthRepository>(create: (context) => AuthService(context.read(), context.read())),
+        RepositoryProvider<SplashCubit>(create: (context) => SplashCubit(context.read())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: lightTheme,
+        title: 'MedicalManagementApp',
+        home: SplashScreen(),
+      ),
     );
   }
 }
