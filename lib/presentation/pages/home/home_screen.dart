85c4ba683e8474f7502b4e6b189503fe17853c90
Christopher Leal <chris_t_o@hotmail.com>
2021-04-13 00:57:29 -0500

    home cubit added to home screen

--- lib/presentation/pages/home/home_screen.dart
+++ lib/presentation/pages/home/home_screen.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medical_management_app/data/repositories/auth_service.dart';
 import 'package:medical_management_app/presentation/pages/login/login_screen.dart';
 import 'package:medical_management_app/config/resources/resources.dart';
 import 'package:medical_management_app/config/utils/utils.dart';
+import 'package:medical_management_app/presentation/pages/medicine/medicine_list.dart';
 import 'package:provider/provider.dart';
+
+import 'home_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('home');
-    return Scaffold(
+    return BlocProvider(
+      create: (context) => HomeCubit(),
+      child: Scaffold(
         appBar: AppBar(
           elevation: 0.0,
           backgroundColor: Colors.transparent,
         ),
-        drawer: _CreateDrawer(),
-        body: Container(
-          child: Center(
-            child: Text('Home screen'),
-          ),
-        ));
+        // drawer: _CreateDrawer(),
+        body: BlocBuilder<HomeCubit, int>(
+          builder: (context, state) {
+            return IndexedStack(
+              index: state,
+              children: [
+                MedicineListScreen(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
 
