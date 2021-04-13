a4ad0d1ae869dbaadf8ed2792e187a3fb0745bf1
Christopher Leal <chris_t_o@hotmail.com>
2021-04-13 00:56:13 -0500

    dependency injection updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medical_management_app/data/repositories/auth_service.dart';
+import 'package:medical_management_app/config/utils/db.dart';
+import 'package:medical_management_app/data/repositories/medicine_service.dart';
 import 'package:medical_management_app/domain/repositories/auth_firebase_repository.dart';
+import 'package:medical_management_app/domain/repositories/medicine_repository.dart';
+import 'package:medical_management_app/presentation/pages/home/home_cubit.dart';
+import 'package:medical_management_app/presentation/pages/medicine/medicine_cubit.dart';
 import 'package:medical_management_app/presentation/pages/splash/splash_cubit.dart';
 import 'package:medical_management_app/presentation/pages/splash/splash_screen.dart';
 
@@ -21,8 +26,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
+        RepositoryProvider<DbHelper>(create: (context) => DbHelper()),
         RepositoryProvider<AuthRepository>(create: (context) => AuthService(FirebaseAuth.instance, FirebaseFirestore.instance)),
+        RepositoryProvider<MedicineRepository>(create: (context) => MedicineService(context.read())),
         RepositoryProvider<SplashCubit>(create: (context) => SplashCubit(context.read())),
+        RepositoryProvider<HomeCubit>(create: (context) => HomeCubit()),
+        RepositoryProvider<MedicineCubit>(create: (context) => MedicineCubit(context.read())),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
