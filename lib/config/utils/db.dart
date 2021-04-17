886cce412c14fbe929816edd58f08f525c812495
Christopher Leal <chris_t_o@hotmail.com>
2021-04-16 20:48:56 -0500

    tables created

--- lib/config/utils/db.dart
+++ lib/config/utils/db.dart
@@ -4,6 +4,7 @@ import 'package:path/path.dart';
 
 const String dbName = 'medical_management.db';
 const String medicinesTableName = 'medicines';
+const String boxesTableName = 'boxes';
 
 class DbHelper {
   static final DbHelper _instance = DbHelper._();
@@ -26,13 +27,24 @@ class DbHelper {
       version: 1,
       onCreate: (db, version) async {
         await db.execute('''
-create table $medicinesTableName ( 
-  id integer primary key autoincrement, 
-  name text not null,
-  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
-  updatedAt TIMESTAMP   
-  )
-''');
+          create table $medicinesTableName ( 
+            id integer primary key autoincrement, 
+            name text not null,
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+            updatedAt TIMESTAMP   
+            )
+        ''');
+        await db.execute('''
+          create table $boxesTableName ( 
+            id integer primary key autoincrement, 
+            medicineId integer not null,           
+            amount DECIMAL(10,2) not null,
+            expirationDate TIMESTAMP,  
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+            updatedAt TIMESTAMP,
+            constraint medicine_box foreign key (medicineId) references $medicinesTableName (id)
+            )
+        ''');
       },
     );
   }
