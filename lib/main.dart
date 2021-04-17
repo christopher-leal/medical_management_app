a6d8457127fbc46fb223038f74b7027807348dd1
Christopher Leal <chris_t_o@hotmail.com>
2021-04-16 20:48:32 -0500

    unused import removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:medical_management_app/data/repositories/auth_service.dart';
-import 'package:medical_management_app/config/utils/db.dart';
-import 'package:medical_management_app/data/repositories/medicine_service.dart';
-import 'package:medical_management_app/domain/repositories/auth_firebase_repository.dart';
-import 'package:medical_management_app/domain/repositories/medicine_repository.dart';
-import 'package:medical_management_app/presentation/pages/home/home_cubit.dart';
-import 'package:medical_management_app/presentation/pages/medicine/medicine_cubit.dart';
-import 'package:medical_management_app/presentation/pages/splash/splash_cubit.dart';
 import 'package:medical_management_app/presentation/pages/splash/splash_screen.dart';
 
 import 'config/theme/theme.dart';
