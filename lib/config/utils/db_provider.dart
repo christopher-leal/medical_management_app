66d6da5270bdeeec3f158ecbc99fff35dc692987
Christopher Leal <chris_t_o@hotmail.com>
2021-04-16 21:23:14 -0500

    db refactor and method created

--- lib/config/utils/db_provider.dart
+++ lib/config/utils/db_provider.dart
@@ -1,3 +1,4 @@
+import 'package:medical_management_app/domain/entities/medicine.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -6,10 +7,10 @@ const String dbName = 'medical_management.db';
 const String medicinesTableName = 'medicines';
 const String boxesTableName = 'boxes';
 
-class DbHelper {
-  static final DbHelper _instance = DbHelper._();
-  factory DbHelper() => _instance;
-  DbHelper._();
+class DBProvider {
+  static final DBProvider _instance = DBProvider._();
+  factory DBProvider() => _instance;
+  DBProvider._();
 
   static Database _database;
   Future<Database> get database async {
@@ -47,5 +48,31 @@ class DbHelper {
         ''');
       },
     );
+  }
+
+  Future<int> setMedicine(Medicine medicine) async {
+    final db = await database;
+    if (medicine.id != null) {
+      return await db.update('$medicinesTableName', medicine.toJson(), where: 'id=?', whereArgs: [medicine.id]);
+    }
+    return await db.insert('$medicinesTableName', medicine.toJson());
+  }
+
+  Future<Medicine> getMedicineById(int id) async {
+    final db = await database;
+    final res = await db.query('$medicinesTableName', where: 'id=?', whereArgs: [id]);
+    return res.isNotEmpty ? Medicine.fromJson(res.first) : null;
+  }
+
+  Future<List<Medicine>> getMedicines() async {
+    final db = await database;
+    var res = await db.query('$medicinesTableName');
+    List<Medicine> medicines = res.isNotEmpty ? res.map((medicine) => Medicine.fromJson(medicine)).toList() : [];
+    return medicines;
+  }
+
+  Future<int> deleteMedicine(int id) async {
+    final db = await database;
+    return await db.delete('$medicinesTableName', where: 'id=?', whereArgs: [id]);
   }
 }
