8fe76852c0f15f247d21d2f388de9349c7bf75b5
Christopher Leal <chris_t_o@hotmail.com>
2021-04-20 02:24:56 -0500

    get box use case created

---
+++ lib/domain/usecases/get_boxes_usecase.dart
@@ -0,0 +1,11 @@
+import 'package:medical_management_app/domain/entities/box.dart';
+import 'package:medical_management_app/domain/repositories/boxes_repository.dart';
+
+class GetBoxesUseCase {
+  GetBoxesUseCase(this._repository);
+  final BoxesRepository _repository;
+
+  Future<List<Box>> getBoxes(int medicineId) async {
+    return await _repository.getBoxesById(medicineId);
+  }
+}
