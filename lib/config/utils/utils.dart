821755bb67f02aaba979c05986a4349fbe26c26c
Christopher Leal <chris_t_o@hotmail.com>
2021-04-19 23:49:30 -0500

    date format static functions

--- lib/config/utils/utils.dart
+++ lib/config/utils/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Utils {
   static void navigateTo(BuildContext context, Widget page) {
@@ -12,5 +13,37 @@ class Utils {
   static void showSnackBar(GlobalKey<ScaffoldState> scaffoldKey, BuildContext context, String text) {
     final snackbar = SnackBar(content: Text(text));
     scaffoldKey.currentState.showSnackBar(snackbar);
+  }
+
+  static String dateWithoutTime(DateTime date) {
+    final DateFormat formatter = DateFormat('yyyy-MM-dd');
+    final String formatted = formatter.format(date);
+    return formatted;
+  }
+
+  static String formatDate({String string, DateTime datetime}) {
+    if (string != null) {
+      DateTime dt0 = DateTime.parse(string);
+      return DateFormat.yMMMMd('es_MX').format(dt0);
+    } else if (datetime != null) {
+      return DateFormat.yMMMMd('es_MX').format(datetime);
+    }
+    return "";
+  }
+
+  static String formatDateNumbers({String string, DateTime datetime}) {
+    if (string != null) {
+      DateTime dt0 = DateTime.parse(string);
+      return DateFormat('d MMM y', 'es_MX').format(dt0);
+    }
+
+    return DateFormat('d MMM y', 'es_MX').format(datetime);
+  }
+
+  static void dismissKeyboard(BuildContext context) {
+    FocusScopeNode cFocus = FocusScope.of(context);
+    if (!cFocus.hasPrimaryFocus) {
+      cFocus.unfocus();
+    }
   }
 }
