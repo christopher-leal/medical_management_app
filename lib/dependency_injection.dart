8e3132bad68ce01596f83166f1468893a9f72fe0
Christopher Leal <chris_t_o@hotmail.com>
2021-04-20 01:04:58 -0500

    new use cases added to the di

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,7 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medical_management_app/data/repositories/boxes_service.dart';
+import 'package:medical_management_app/domain/repositories/boxes_repository.dart';
 import 'package:medical_management_app/domain/usecases/medicines_usecase.dart';
+import 'package:medical_management_app/domain/usecases/save_boxes_usecase.dart';
+import 'package:medical_management_app/domain/usecases/save_medicine_usecase.dart';
 
 import 'config/utils/db_provider.dart';
 import 'data/repositories/auth_service.dart';
@@ -14,6 +18,9 @@ List<RepositoryProvider<Object>> injectDependencies() => [
       RepositoryProvider<DBProvider>(create: (context) => DBProvider()),
       RepositoryProvider<AuthRepository>(create: (context) => AuthService(FirebaseAuth.instance, FirebaseFirestore.instance)),
       RepositoryProvider<MedicineRepository>(create: (context) => MedicineService(context.read())),
+      RepositoryProvider<BoxesRepository>(create: (context) => BoxesService(context.read())),
       // useCases
       RepositoryProvider<MedicinesUseCase>(create: (context) => MedicinesUseCase(context.read())),
+      RepositoryProvider<SaveMedicineUseCase>(create: (context) => SaveMedicineUseCase(context.read())),
+      RepositoryProvider<SaveBoxesUseCase>(create: (context) => SaveBoxesUseCase(context.read())),
     ];
